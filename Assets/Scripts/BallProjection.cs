cebf3cbcdad7343090f808697ae1584bce9f22cb
Kablion <kablion_97@yahoo.de>
2020-07-05 12:09:01 +0200

    WallProjection Implemented

--- Assets/Scripts/BallProjection.cs
+++ Assets/Scripts/BallProjection.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class BallProjection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject sideWallProjection;
+    public GameObject horizonatalWallProjection;
+    public GameObject verticalWallProjection;
 
     // Update is called once per frame
     void Update()
     {
-        
+        sideWallProjection.transform.SetPositionAndRotation(new Vector3(0,0,transform.position.z),Quaternion.identity);
+        horizonatalWallProjection.transform.SetPositionAndRotation(new Vector3(0,transform.position.y,0),Quaternion.identity);
+        verticalWallProjection.transform.SetPositionAndRotation(new Vector3(transform.position.x,0,0),Quaternion.identity);
     }
 }
